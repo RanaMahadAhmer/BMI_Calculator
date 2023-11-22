e6e36dd2eba3b05916b375407c2ff3c51cf2b1f5
Rana Mahad Ahmer <ranamahadahmer@outlook.com>
2023-11-22 10:06:01 +0500

    Modularized Code

--- lib/screens/bmiScreen.dart
+++ lib/screens/bmiScreen.dart
@@ -16,6 +16,59 @@ class BmiScreen extends StatefulWidget {
 }
 
 class _BmiScreenState extends State<BmiScreen> {
+  Widget _button() {
+    return MainButton(
+      msg: 'Calculate',
+      onTap: () {
+        double bmi;
+        String result;
+        String? msg;
+        (bmi, result, msg) = calculateBmi(
+            height: DefaultParamValues.height,
+            weight: DefaultParamValues.weight);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ResultScreen(
+                  bmi: bmi.toStringAsFixed(2), result: result, msg: msg!)),
+        );
+      },
+    );
+  }
+
+  Widget _landscapeBuild() {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 25,
+        title: const Text(
+          "BMI",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+      body: addPadding(
+        wid: Row(
+          children: [
+            Expanded(flex: 3, child: addPadding(wid: const GenderCardRow())),
+            Expanded(flex: 3, child: addPadding(wid: const WeightAgeRow())),
+            Expanded(
+                flex: 6,
+                child: Column(
+                  children: [
+                    Expanded(
+                        flex: 3, child: addPadding(wid: const HeightSlider())),
+                    Expanded(
+                      child: addPadding(
+                        wid: _button(),
+                      ),
+                    ),
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return isPortraitMode(context)
@@ -38,93 +91,13 @@ class _BmiScreenState extends State<BmiScreen> {
                       flex: 3, child: addPadding(wid: const WeightAgeRow())),
                   Expanded(
                     child: addPadding(
-                      wid: MainButton(
-                        msg: 'Calculate',
-                        onTap: () {
-                          double bmi;
-                          String result;
-                          String? msg;
-                          (bmi, result, msg) = calculateBmi(
-                              height: DefaultParamValues.height,
-                              weight: DefaultParamValues.weight);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ResultScreen(
-                                    bmi: bmi.toStringAsFixed(2),
-                                    result: result,
-                                    msg: msg!)),
-                          );
-                        },
-                      ),
+                      wid: _button(),
                     ),
                   ),
                 ],
               ),
             ),
           )
-        : const LandscapeBmiScreen();
-  }
-}
-
-class LandscapeBmiScreen extends StatefulWidget {
-  const LandscapeBmiScreen({super.key});
-
-  @override
-  State<LandscapeBmiScreen> createState() => _LandscapeBmiScreenState();
-}
-
-class _LandscapeBmiScreenState extends State<LandscapeBmiScreen> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: Implement Landscape Mode for the app
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 25,
-        title: const Text(
-          "BMI",
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-      body: addPadding(
-        wid: Row(
-          children: [
-            Expanded(flex: 3, child: addPadding(wid: const GenderCardRow())),
-            Expanded(flex: 3, child: addPadding(wid: const WeightAgeRow())),
-            Expanded(
-                flex: 6,
-                child: Column(
-                  children: [
-                    Expanded(
-                        flex: 3, child: addPadding(wid: const HeightSlider())),
-                    Expanded(
-                      child: addPadding(
-                        wid: MainButton(
-                          msg: 'Calculate',
-                          onTap: () {
-                            double bmi;
-                            String result;
-                            String? msg;
-                            (bmi, result, msg) = calculateBmi(
-                                height: DefaultParamValues.height,
-                                weight: DefaultParamValues.weight);
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ResultScreen(
-                                      bmi: bmi.toStringAsFixed(2),
-                                      result: result,
-                                      msg: msg!)),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                )),
-          ],
-        ),
-      ),
-    );
+        : _landscapeBuild();
   }
 }
